931f7624b0541df8c3d9e7d99eba6a2b69cebdba
Jake Deichert <jakedeichert@users.noreply.github.com>
2016-08-25 12:54:54 -0400

    Add example tests

---
+++ test/test.js
@@ -0,0 +1,15 @@
+import { expect } from 'chai';
+
+describe('Example tests...', function() {
+    it('true should equal true', () => {
+        expect(true).to.equal(true);
+    });
+
+    it('null should be null', () => {
+        expect(null).to.be.null;
+    });
+
+    it('"react starter" should contain "start"', () => {
+        expect('react starter').to.contain('start');
+    });
+});
